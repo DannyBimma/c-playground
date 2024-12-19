6e49854d8b0893064b0b08aae158cb4a3c77a356
DannyBimma <dantrotman92@gmail.com>
2024-12-19 10:57:35 -0400

    refactor code

--- hello.c
+++ hello.c
@@ -4,35 +4,37 @@
 
 int main() 
 {
-    char name[50]; // Init input strings (50 and 4 chars max)
+    char name[50];
     char res[4];
-    int resVal = -1; // Init res val
+
   
-    printf("Enter your name: "); // Get user input
-    fgets(name, sizeof(name), stdin); // Read chars (49 max) from std input
+  // Get and format inital user input
+    printf("Enter your name: "); 
+
+    fgets(name, sizeof(name), stdin);
     name[strcspn(name, "\n")] = '\0'; // Remove new line from fgets
     
     printf("Hello, %s!\n", name);
 
-    // Ask user the question and format input
+    // Print "the question" and format user input
     printf("Do you believe? \n");
+
     fgets(res, sizeof(res), stdin);
     res[strcspn(res, "\n")] = '\0';
     
-    // Convert input to lowercase
     for (int i = 0; res[i]; i++) 
     {
         res[i] = tolower(res[i]);
     }
 
-    // Validate res
+    // Validate input and print msg
     if (strcmp(res, "yes") == 0 || strcmp(res, "y") == 0) 
     {
-        resVal = 1;
+        printf("Welcome to the Matrix, %s. We've been expecting you 😎 !!\n", name);
     } 
     else if (strcmp(res, "no") == 0 || strcmp(res, "n") == 0) 
     {
-        resVal = 0;
+        printf("You are not ready, %s. You remain blind. Sad to see 😔 !!\n", name);
     } 
     else 
     {
@@ -40,15 +42,5 @@ int main()
         return 1;
     }
 
-    // Print custom msg for res
-    if(resVal == 1)
-    {
-        printf("Welcome to the Matrix, %s. We've been expecting you 😎 !!\n", name);
-    }
-    else
-    {
-        printf("You are not ready, %s. You remain blind. Sad to see 😔 !!\n", name);
-    }
-    
     return 0;
 }
