a2475dfe87ce5563c53315598bffd0dabc6c7ea1
DannyBimma <dantrotman92@gmail.com>
2024-12-29 12:22:58 -0400

    I AM SO FUCKING STUPIDgit status

--- blocks.c
+++ blocks.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
-#include <cs50.h>
 
 int main(void)
 {
-    int block_h = 3;
-    int block_w = 3;
-
-    for(int i = 0; i < block_h; i++)
+    int block_h, block_w;
+    
+    // Get height input
+    printf("Input block height: ");
+    while (scanf("%d", &block_h) != 1 || block_h <= 0)
     {
-        for(int j = 0; j < block_w; j++)
+        // Clear input buffer in case of invalid input
+        while (getchar() != '\n');
+        printf("Please enter a valid positive number for height: ");
+    }
+    
+    // Get width input
+    printf("Input block width: ");
+    while (scanf("%d", &block_w) != 1 || block_w <= 0)
+    {
+        while (getchar() != '\n');
+        printf("Please enter a valid positive number for width: ");
+    }
+    
+    // Print the block pattern
+    for (int i = 0; i < block_h; i++)
+    {
+        for (int j = 0; j < block_w; j++)
         {
             printf("#");
         }
         printf("\n");
     }
+    
+    return 0;
 }
