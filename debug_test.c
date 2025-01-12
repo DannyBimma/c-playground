aace93fef3df2c8898af908d02f3d5e00720de44
DannyBimma <dantrotman92@gmail.com>
2025-01-12 19:13:29 -0400

    install and test the LLDB debugger

---
+++ debug_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int multiply(int a, int b) {
+    int result = a * b;
+    return result;  // breakpoint
+}
+
+int main() {
+    int x = 5;
+    int y = 7;
+    int product = multiply(x, y);
+    printf("Result: %d\n", product);
+    return 0;
+}
