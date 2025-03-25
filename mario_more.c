5e429856d2a87bdc6486b5f5af8ae9eccc89f5fb
DannyBimma <dantrotman92@gmail.com>
2025-03-25 12:30:40 -0400

    cs50 mario-more solution

--- mario_more.c
+++ mario_more.c
@@ -5,7 +5,50 @@
 int main(void)
 {
     int height;
+    bool valid_input = false;
 
-    // Validate input
-    
+    // Input validation
+    do
+    {
+        printf("Height: ");
+        if (scanf("%d", &height) == 1)
+        {
+            if (height >= 1 && height <= 8)
+            {
+                valid_input = true;
+            }
+        }
+        // Clear input buffer
+        while (getchar() != '\n');
+    }
+    while (!valid_input);
+
+    // Pyramid scheme
+    for (int i = 1; i <= height; i++)
+    {
+        // Print preceding spaces
+        for (int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+        
+        // Print first pyramid
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        
+        // Print gap between pyramids
+        printf("  ");
+        
+        // Print second pyramid
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
 }
