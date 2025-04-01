a74cba4a16c7b63241482b1393f5d31dd3a3a693
DannyBimma <dantrotman92@gmail.com>
2025-04-01 17:15:04 -0400

    start cs50 scrabble solution

---
+++ scrabble.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+// Letter values
+int const A = 1;
+int const B = 3;
+int const C = 3;
+int const D = 2;
+int const E = 1;
+int const F = 4;
+int const G = 2;
+int const H = 4;
+int const I = 1;
+int const J = 8;
+int const K = 5;
+int const L = 1;
+int const M = 3;
+int const N = 1;
+int const O = 1;
+int const P = 3;
+int const Q = 10;
+int const R = 1;
+int const S = 1;
+int const T = 1;
+int const U = 1;
+int const V = 4;
+int const W = 4;
+int const X = 8;
+int const Y = 4;
+int const Z = 10;
+
+int main(void)
+{
+
+}
