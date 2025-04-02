7aa7f730f80ff63df2a3dc7cfcc6e37b94dab00e
DannyBimma <dantrotman92@gmail.com>
2025-04-02 17:07:08 -0400

    update cs50_mario_less

--- cs50_mario_less.c
+++ cs50_mario_less.c
@@ -1,70 +1,37 @@
+#include <cs50.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <limits.h> // for INT_MAX macro
-
-// Helper funcs
-void print_rows(int i, int h);
-bool int_validator(int input);
-int get_int(const char *input);
+#include <limits.h> // IDK, seems like a good idea for mem safety
 
 
 int main(void)
 {
     int height;
 
-    // Validate input
+    // Prompt user for a number between 1 and... the end of a 32-bit integer 🤷🏽‍♂️?
     do
     {
         height = get_int("HEIGHT: ");
     }
-    while (int_validator(height));
+    while (height < 1 || height > INT_MAX);
 
-    // print pyramid based height
+    // Print pyramid rows based height
     for (int i = 0; i < height; i++)
     {
-        print_rows(i, height);
 
+        // Print spaces for each row (1-less than height)
+        for (int k = i; k < (height - 1); k++)
+        {
+            printf(" ");
+        }
+
+        // Print pyramid colums based on row interation
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        
+        // Escape to new line
         printf("\n");
+
     }
-}
-
-
-
-// Use "height" input to print a row of spaces 1-less than the block's height
-// Print a "#" at the end of each row of spaces
-// Create the pyramid effect ☺️
-void print_rows(int i, int h)
-{
-    for (int j = i; j < (h - 1); j++)
-    {
-        printf(" ");
-    }
-
-    for (int k = 0; k <= i; k++)
-    {
-        printf("#");
-    }
-}
-
-// Check dem fools input son
-bool int_validator(int input)
-{
-    if (input <= 0 || input >= INT_MAX)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-// Get user input of type int
-int get_int(const char *input)
-{
-    int num;
-    printf("%s", input);
-    scanf("%d", &num);
-    
-    return num;
 }
