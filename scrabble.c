dce2d7f2a0591f69f41ca16c5d142020c9ddd9be
DannyBimma <dantrotman92@gmail.com>
2025-04-01 21:52:25 -0400

    abstract away scoring adding functionality

--- scrabble.c
+++ scrabble.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <ctype.h>
 
+// Function prototypes
+int score_calc(string word);
+void to_uppercase(string word);
+
 // Scrabble letter values
 int letter_val_arr[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
@@ -10,38 +14,19 @@ int main(void)
 {
     int player_1_score = 0;
     int player_2_score = 0;
-
+    
     // Get user input
     string word_1 = get_string("Player 1: ");
     string word_2 = get_string("Player 2: ");
-
+    
     // Convert to uppercase
-    for (int i = 0; i < strlen(word_1); i++)
-    {
-        word_1[i] = toupper(word_1[i]);
-    }
-    for (int i = 0; i < strlen(word_2); i++)
-    {
-        word_2[i] = toupper(word_2[i]);
-    }
-
+    to_uppercase(word_1);
+    to_uppercase(word_2);
+    
     // Calculate score for each word
-    for (int i = 0; i < strlen(word_1); i++)
-    {
-        if (isalpha(word_1[i]))
-        {
-            player_1_score += letter_val_arr[word_1[i] - 'A'];
-        }
-    }
-
-    for (int i = 0; i < strlen(word_2); i++)
-    {
-        if (isalpha(word_2[i]))
-        {
-            player_2_score += letter_val_arr[word_2[i] - 'A'];
-        }
-    }
-
+    player_1_score = score_calc(word_1);
+    player_2_score = score_calc(word_2);
+    
     // Print scores
     if (player_1_score > player_2_score)
     {
@@ -55,7 +40,33 @@ int main(void)
     {
         printf("Tie!\n");
     }
-
+    
     return 0;
+}
 
+// Score calc function
+int score_calc(string word)
+{
+    int score = 0;
+    int length = strlen(word);
+    
+    for (int i = 0; i < length; i++)
+    {
+        if (isalpha(word[i]))
+        {
+            score += letter_val_arr[word[i] - 'A'];
+        }
+    }
+    return score;
+}
+
+// Uppercase function
+void to_uppercase(string word)
+{
+    int length = strlen(word);
+    
+    for (int i = 0; i < length; i++)
+    {
+        word[i] = toupper(word[i]);
+    }
 }
