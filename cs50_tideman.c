815c6830b304057a7f94a12ae84bf8ec2d81acd3
DannyBimma <dantrotman92@gmail.com>
2025-04-15 21:32:52 -0400

    Ahhhgrgrgfggf... FUCKgit status

--- cs50_tideman.c
+++ cs50_tideman.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+
 // Max number of candidates
 #define MAX 9
 
@@ -31,7 +32,6 @@ void add_pairs(void);
 void sort_pairs(void);
 void lock_pairs(void);
 void print_winner(void);
-bool has_cycle(int start, int end, bool visited[]);
 
 int main(int argc, string argv[])
 {
@@ -117,7 +117,9 @@ void record_preferences(int ranks[])
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            preferences[ranks[i]][ranks[j]]++;
+            int pref_i = ranks[i]; // Earlier preference
+            int pref_j = ranks[j]; // Later preference
+            preferences[pref_i][pref_j]++;
         }
     }
 }
@@ -125,13 +127,23 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
+    pair_count = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            pairs[pair_count].winner = i;
-            pairs[pair_count].loser = j;
-            pair_count++;
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[j][i] > preferences[i][j])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
         }
     }
 }
@@ -139,13 +151,12 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // Bubble sort: compare adjacent pairs and swap if need be
     for (int i = 0; i < pair_count - 1; i++)
     {
         for (int j = 0; j < pair_count - i - 1; j++)
         {
-            int strength1 = preferences[pairs[j].winner][pairs[j].loser];
-            int strength2 = preferences[pairs[j + 1].winner][pairs[j + 1].loser];
+            int strength1 = preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner];
+            int strength2 = preferences[pairs[j + 1].winner][pairs[j + 1].loser] - preferences[pairs[j + 1].loser][pairs[j + 1].winner];
 
             if (strength1 < strength2)
             {
@@ -187,7 +198,6 @@ void lock_pairs(void)
     for (int i = 0; i < pair_count; i++)
     {
         bool visited[MAX] = {false};
-        // Before adding the edge, check if it would create a cycle
         if (!has_cycle(pairs[i].loser, pairs[i].winner, visited))
         {
             locked[pairs[i].winner][pairs[i].loser] = true;
@@ -198,15 +208,26 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
+    // The winner is the source of the graph (has no incoming edges)
     for (int i = 0; i < candidate_count; i++)
     {
+        bool is_source = true;
+
+        // Check if candidate i has any incoming edges
         for (int j = 0; j < candidate_count; j++)
         {
-            if (locked[i][j])
+            if (locked[j][i])
             {
-                printf("%s\n", candidates[i]);
-                return;
+                is_source = false;
+                break;
             }
+        }
+
+        // If no incoming edges found, this is our winner
+        if (is_source)
+        {
+            printf("%s\n", candidates[i]);
+            return;
         }
     }
 }
