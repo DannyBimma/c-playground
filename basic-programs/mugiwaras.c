02e3bb45a83f8f6105e0106f970fb4068b864614
DannyBimma <dantrotman92@gmail.com>
2025-09-14 21:39:45 -0400

    Init program.

---
+++ basic-programs/mugiwaras.c
@@ -0,0 +1,8 @@
+/*
+ * Author: Technomancer Pirate Captain
+ * Purpose: Identify the members of Strawhat's flagship
+ * Copyright (c) 2025 DannyBimma. All Rights Reserved.*/
+
+#include <stdio.h>
+
+int main(void) {}
