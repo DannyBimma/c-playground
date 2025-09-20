71a761a5f2123e40e409d4c5c2bd1a17a0d3122f
DannyBimma <dantrotman92@gmail.com>
2025-09-19 20:14:55 -0400

    Meh.

--- basic-programs/ctoi.c
+++ basic-programs/ctoi.c
@@ -4,8 +4,8 @@ int main(void) {
   signed char c = -5; // 8-bits
   int i = c;          // 32-bits
 
-  printf("char 'c' cast to int: %d\n", i);
   // Output: -5
+  printf("char c cast to int i = %d\n", i);
 
   return 0;
 }
