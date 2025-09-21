9d6234a74c39cedfb80a6a131e22e34c9fa56b57
DannyBimma <dantrotman92@gmail.com>
2025-09-21 18:20:13 -0400

    Fix program info.

--- basic-programs/year2mindays.c
+++ basic-programs/year2mindays.c
@@ -1,6 +1,6 @@
 /*
- * Routine: A program that converts a number of years into an exact
- * amount of days and minutes. A bit stupid, but practice is practice
+ * Routine: A program that converts a number of minutes into an exact
+ * amount of years and days. A bit stupid, but practice is practice
  * I guess 🤷🏽‍♂️
  * Author: DannyBimma
  * Copyright: (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
