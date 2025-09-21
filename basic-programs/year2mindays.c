9997d66c1b6ddd3aadf3e66647d5f47d5f7b1e74
DannyBimma <dantrotman92@gmail.com>
2025-09-21 18:36:18 -0400

    Write the minute converter.

--- basic-programs/year2mindays.c
+++ basic-programs/year2mindays.c
@@ -5,3 +5,26 @@
  * Author: DannyBimma
  * Copyright: (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
  */
+
+#include <stdio.h>
+
+int main(void) {
+  long double mins;
+  long double years, days;
+
+  printf("\n");
+  printf("---- THE ULTIMATE MINUTE CONVERTER V365.24 ----\n");
+  printf("Enter the a of minutes: ");
+  scanf("%Lf", &mins);
+
+  years = mins / (365.25 * 24 * 60);
+  days = mins / (24 * 60);
+
+  printf("\n");
+  printf("%.2Lf minutes is equal to:\n", mins);
+  printf("%.6Lf years\n", years);
+  printf("%.6Lf days\n", days);
+  printf("\n");
+
+  return 0;
+}
