ef8eb59eeba8f6ffc0ccccc07583c33dca87e2af
DannyBimma <dantrotman92@gmail.com>
2025-09-21 18:18:00 -0400

    Init program file.

---
+++ basic-programs/year2mindays.c
@@ -0,0 +1,7 @@
+/*
+ * Routine: A program that converts a number of years into an exact
+ * amount of days and minutes. A bit stupid, but practice is practice
+ * I guess 🤷🏽‍♂️
+ * Author: DannyBimma
+ * Copyright: (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ */
