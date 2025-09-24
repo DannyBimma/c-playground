c203682b5b11f5d8d565b7a7ca2096f583672f85
DannyBimma <dantrotman92@gmail.com>
2025-09-24 06:43:15 -0400

    Check for uint128.

--- basic-programs/size.c
+++ basic-programs/size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void) {
-  char *s = "I am the man who will become The King of the Pirates!!!!";
+  __uint128_t val;
 
   printf("The size of an int is: %lu\n", sizeof(int));
   printf("The size of a float is: %lu\n", sizeof(float));
@@ -10,10 +9,7 @@ int main(void) {
   printf("The size of a long long is: %lu\n", sizeof(long long));
   printf("The size of a double is: %lu\n", sizeof(double));
   printf("The size of a long double is: %lu\n", sizeof(long double));
-  printf("The size of a char is: %lu\n", sizeof(char));
-  printf("The string s is: %s\n", s);
-  printf("The size of the string s is: %lu\n", sizeof(s));
-  printf("But the length of string s is: %lu\n", strlen(s));
+  printf("uint128 var is: %lu\n", sizeof(val));
 
   return 0;
 }
