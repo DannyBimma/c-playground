74dbc30a414b9f8c9dcd4c47c316e1596fb61e80
DannyBimma <dantrotman92@gmail.com>
2025-09-23 21:41:06 -0400

    Format.

--- basic-programs/array_counter.c
+++ basic-programs/array_counter.c
@@ -1,27 +1,39 @@
+/*
+ * Routine: A program that counts occurrences of each digit
+ * (0-9 with the count matched to their respective array index,
+ * white space characters, and other misc characters from input.
+ * Author: DannyBimma
+ * Copyright: (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ */
+
 #include <stdio.h>
 
 int main(void) {
-    int n_white = 0, n_other = 0;
-    int n_digit[10];
-    int c;
+  int n_white = 0, n_other = 0;
+  int n_digit[10];
+  int c;
 
-    for(int i = 0; i < 10; i++)
-        n_digit[i] = 0;
+  // Zero out counter array
+  for (int i = 0; i < 10; i++)
+    n_digit[i] = 0;
 
-    while((c = getchar()) != EOF) {
-        if (c >= '0' && c <= '9')
-            ++n_digit[c-'0'];
-        else if (c == ' ' || c == '\n' || c == '\t')
-            ++n_white;
-        else
-            ++n_other;
-    }
-    printf("digits = ");
+  // Process each char from stdin until EOF
+  while ((c = getchar()) != EOF) {
+    if (c >= '0' && c <= '9')
+      ++n_digit[c - '0']; // increment counter for this specific digit
+    else if (c == ' ' || c == '\n' || c == '\t')
+      ++n_white;
+    else
+      ++n_other;
+  }
 
-    for(int i = 0; i < 10; i++)
-        printf(" %d", n_digit[i]);
+  // Print results
+  printf("digits = ");
 
-    printf(", white space = %d, other = %d\n", n_white, n_other);
+  for (int i = 0; i < 10; i++)
+    printf(" %d", n_digit[i]);
 
-    return 0;
+  printf(", white space = %d, other = %d\n", n_white, n_other);
+
+  return 0;
 }
