cc4b959491f8e4070c4a46c1bd80231a7ae7c701
DannyBimma <dantrotman92@gmail.com>
2025-09-23 21:55:15 -0400

    Format.

--- basic-programs/factorials.c
+++ basic-programs/factorials.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int factorial(int n);
 
-int main(void)
-{
-    int n = get_int("Input a number: ");
-    printf("The factorial of %i is %i\n", n, factorial(n));
+int main(void) {
+  int n = get_int("Input a number: ");
+
+  printf("The factorial of %i is %i\n", n, factorial(n));
 }
 
-int factorial(int n)
-{
-    if (n == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return n * factorial(n - 1);
-    }
+int factorial(int n) {
+  if (n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
