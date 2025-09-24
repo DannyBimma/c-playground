5f40cf0d2b1e3cf312bb5c54e49efe65d5cb6e39
DannyBimma <dantrotman92@gmail.com>
2025-09-23 21:53:14 -0400

    Format.

--- basic-programs/debug_test.c
+++ basic-programs/debug_test.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int multiply(int a, int b) {
-    int result = a * b;
-    return result;  // breakpoint
+  int result = a * b;
+
+  return result; // breakpoint
 }
 
 int main() {
-    int x = 5;
-    int y = 7;
-    int product = multiply(x, y);
-    printf("Result: %d\n", product);
-    return 0;
+  int x = 5;
+  int y = 7;
+  int product = multiply(x, y);
+
+  printf("Result: %d\n", product);
+
+  return 0;
 }
