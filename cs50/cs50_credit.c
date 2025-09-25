875e03317b00ccd1ca2ec9bd586cc8b558825e3b
DannyBimma <dantrotman92@gmail.com>
2025-09-24 21:57:55 -0400

    Format.

--- cs50/cs50_credit.c
+++ cs50/cs50_credit.c
@@ -1,87 +1,76 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-#include <cs50.h>
-
 
 bool luhns_algo(long card_num);
 string card_checker(long num);
 
-int main(void)
-{
-    long card_num = get_long("Number: ");
+int main(void) {
+  long card_num = get_long("Number: ");
 
-    // Validation
-    if (!luhns_algo(card_num))
-    {
-        printf("INVALID\n");
-        return 0;
-    }
-
-    // Result
-    string card_type = card_checker(card_num);
-    printf("%s\n", card_type);
+  // Validation
+  if (!luhns_algo(card_num)) {
+    printf("INVALID\n");
 
     return 0;
-}
+  }
 
+  // Result
+  string card_type = card_checker(card_num);
+  printf("%s\n", card_type);
+
+  return 0;
+}
 
 // Luhn's Algorithm
-bool luhns_algo(long card_num)
-{
-    int sum = 0;
-    bool flag = false;
+bool luhns_algo(long card_num) {
+  int sum = 0;
+  bool flag = false;
 
-    // Cast long to string
-    char num_str[20];
-    sprintf(num_str, "%ld", card_num);
-    int num_len = strlen(num_str);
+  // Cast long to string
+  char num_str[20];
+  sprintf(num_str, "%ld", card_num);
+  int num_len = strlen(num_str);
 
-    // Iterate starting from end
-    for (int i = num_len - 1; i >= 0; i--)
-    {
-        // Convert char to int
-        int digit = num_str[i] - '0';
+  // Iterate starting from end
+  for (int i = num_len - 1; i >= 0; i--) {
+    // Convert char to int
+    int digit = num_str[i] - '0';
 
-        if (flag)
-        {
-            digit *= 2;
-            // Add digits separately for two digit results
-            sum += (digit % 10) + (digit / 10);
-        }
-        else
-        {
-            sum += digit;
-        }
-
-        // Flip flag
-        flag = !flag;
+    if (flag) {
+      digit *= 2;
+      // Add digits separately for two digit results
+      sum += (digit % 10) + (digit / 10);
+    } else {
+      sum += digit;
     }
 
-    return (sum % 10 == 0);
+    // Flip flag
+    flag = !flag;
+  }
+
+  return (sum % 10 == 0);
 }
 
+string card_checker(long num) {
+  // Cast long to string
+  char num_str[20];
+  sprintf(num_str, "%ld", num);
+  int len = strlen(num_str);
 
-string card_checker(long num)
-{
-    // Cast long to string
-    char num_str[20];
-    sprintf(num_str, "%ld", num);
-    int len = strlen(num_str);
+  if (len == 15 &&
+      (strncmp(num_str, "34", 2) == 0 || strncmp(num_str, "37", 2) == 0)) {
+    return "AMEX";
+  }
 
-    if (len == 15 && (strncmp(num_str, "34", 2) == 0 || strncmp(num_str, "37", 2) == 0))
-    {
-        return "AMEX";
-    }
+  if (len == 16 && num_str[0] == '5' && num_str[1] >= '1' &&
+      num_str[1] <= '5') {
+    return "MASTERCARD";
+  }
 
-    if (len == 16 && num_str[0] == '5' && num_str[1] >= '1' && num_str[1] <= '5')
-    {
-        return "MASTERCARD";
-    }
+  if ((len == 13 || len == 16) && num_str[0] == '4') {
+    return "VISA";
+  }
 
-    if ((len == 13 || len == 16) && num_str[0] == '4')
-    {
-        return "VISA";
-    }
-
-    return "INVALID";
+  return "INVALID";
 }
