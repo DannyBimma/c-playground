c7424124a9bb4879fbabf68f1ae5dfa2ef7298b6
DannyBimma <dantrotman92@gmail.com>
2025-09-24 21:56:51 -0400

    Format.

--- cs50/cs50_caesar.c
+++ cs50/cs50_caesar.c
@@ -1,68 +1,58 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 bool digits(string arg);
 char rotate(char p, int k);
 
-int main(int argc, string argv[])
-{
-    // Input validation
-    if (argc != 2 || !digits(argv[1]))
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
-    }
+int main(int argc, string argv[]) {
+  // Input validation
+  if (argc != 2 || !digits(argv[1])) {
+    printf("Usage: ./caesar key\n");
 
-    // Convert input arg to int
-    int k = atoi(argv[1]);
+    return 1;
+  }
 
-    string plaintext = get_string("plaintext:  ");
+  // Convert input arg to int
+  int k = atoi(argv[1]);
 
-    // Label
-    printf("ciphertext: ");
+  string plaintext = get_string("plaintext:  ");
 
-    // Rotate and print each char in plaintext using key
-    for (int i = 0, l = strlen(plaintext); i < l; i++)
-    {
-        printf("%c", rotate(plaintext[i], k));
-    }
+  // Label
+  printf("ciphertext: ");
 
-    printf("\n");
-    return 0;
+  // Rotate and print each char in plaintext using key
+  for (int i = 0, l = strlen(plaintext); i < l; i++) {
+    printf("%c", rotate(plaintext[i], k));
+  }
+
+  printf("\n");
+
+  return 0;
 }
 
 // Check if input arg is all digits
-bool digits(string arg)
-{
-    for (int i = 0, l = strlen(arg); i < l; i++)
-    {
-        if (!isdigit(arg[i]))
-        {
-            return false;
-        }
+bool digits(string arg) {
+  for (int i = 0, l = strlen(arg); i < l; i++) {
+    if (!isdigit(arg[i])) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 // Rotate and wrap chars by key
-char rotate(char p, int k)
-{
-    if (isupper(p))
-    {
-        // Rotate uppercase letter and return
-        return ((p - 'A' + k) % 26) + 'A';
-    }
-    else if (islower(p))
-    {
-        // Rotate lowercase letter and return
-        return ((p - 'a' + k) % 26) + 'a';
-    }
-    else
-    {
-        return p;
-    }
+char rotate(char p, int k) {
+  if (isupper(p)) {
+    // Rotate uppercase letter and return
+    return ((p - 'A' + k) % 26) + 'A';
+  } else if (islower(p)) {
+    // Rotate lowercase letter and return
+    return ((p - 'a' + k) % 26) + 'a';
+  } else {
+    return p;
+  }
 }
