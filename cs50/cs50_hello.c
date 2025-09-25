61f4b57879799db50a7ca34f616486dfdab7f415
DannyBimma <dantrotman92@gmail.com>
2025-09-24 21:58:39 -0400

    Format.

--- cs50/cs50_hello.c
+++ cs50/cs50_hello.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-
-int main(void)
-{
-    // You know what, CS50 faculty? Deal with it 🙄!!
-    printf("hello, %s\n", get_string("What's your name? "));
+int main(void) {
+  // You know what, CS50 faculty? Deal with it 🙄!!
+  printf("hello, %s\n", get_string("What's your name? "));
 }
