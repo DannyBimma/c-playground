68ccef13aab4d6e5d2ef33aa97bf72b63902df8d
DannyBimma <dantrotman92@gmail.com>
2025-09-24 22:01:24 -0400

    Format.

--- cs50/cs50_mario_more.c
+++ cs50/cs50_mario_more.c
@@ -1,53 +1,45 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-int main(void)
-{
-    int height;
-    bool valid_input = false;
+int main(void) {
+  int height;
+  bool valid_input = false;
 
-    // Input validation
-    do
-    {
-        printf("Height: ");
-        if (scanf("%d", &height) == 1)
-        {
-            if (height >= 1 && height <= 8)
-            {
-                valid_input = true;
-            }
-        }
-        // Clear input buffer
-        while (getchar() != '\n');
+  // Input validation
+  do {
+    printf("Height: ");
+    if (scanf("%d", &height) == 1) {
+      if (height >= 1 && height <= 8) {
+        valid_input = true;
+      }
     }
-    while (!valid_input);
+    // Clear input buffer
+    while (getchar() != '\n')
+      ;
+  } while (!valid_input);
 
-    // Pyramid scheme
-    for (int i = 1; i <= height; i++)
-    {
-        // Print preceding spaces
-        for (int j = 0; j < height - i; j++)
-        {
-            printf(" ");
-        }
-        
-        // Print first pyramid
-        for (int j = 0; j < i; j++)
-        {
-            printf("#");
-        }
-        
-        // Print gap between pyramids
-        printf("  ");
-        
-        // Print second pyramid
-        for (int j = 0; j < i; j++)
-        {
-            printf("#");
-        }
-        
-        printf("\n");
+  // Pyramid scheme
+  for (int i = 1; i <= height; i++) {
+    // Print preceding spaces
+    for (int j = 0; j < height - i; j++) {
+      printf(" ");
     }
 
-    return 0;
+    // Print first pyramid
+    for (int j = 0; j < i; j++) {
+      printf("#");
+    }
+
+    // Print gap between pyramids
+    printf("  ");
+
+    // Print second pyramid
+    for (int j = 0; j < i; j++) {
+      printf("#");
+    }
+
+    printf("\n");
+  }
+
+  return 0;
 }
