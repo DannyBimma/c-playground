3ccec30b63ff20b31a937f1e7c9c66d8a777743b
DannyBimma <dantrotman92@gmail.com>
2025-09-25 21:11:14 -0400

    Init program.

---
+++ basic-programs/wage.c
@@ -0,0 +1,6 @@
+/*
+ * Routine: A program that calculates the amount of money paid to the
+ * user on a weekly basis.
+ * Author: DannyBimma
+ * Copyright: (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ */
