74b191a34a761f533e8feca6197df550c84482c5
DannyBimma <dantrotman92@gmail.com>
2025-09-25 22:08:44 -0400

    Fix ya math, do ya taxes.

--- basic-programs/wage.c
+++ basic-programs/wage.c
@@ -9,25 +9,59 @@
 
 #define PAYRATE 12.00
 
+// Prototypes
+double calculate_tax(double wage);
+
 int main(void) {
-  int hrsWorked;
-  int taxes;
-  int grossPay;
-  int netPay;
+  double hrsWorked;
+  double taxes;
+  double grossPay;
+  double netPay;
 
   // Get the amount of hrs worked
   printf("Enter number of hours worked this week:\t");
-  scanf("%d", &hrsWorked);
+  scanf("%lf", &hrsWorked);
 
   // Calc basic pay & overtime before taxes
   if (hrsWorked > 40) {
-    int overTimeRate = PAYRATE * 1.5;
-    grossPay = hrsWorked * overTimeRate;
+    double overTimeRate = PAYRATE * 1.5;
+    grossPay = (40 * PAYRATE) + ((hrsWorked - 40) * overTimeRate);
   } else {
     grossPay = hrsWorked * PAYRATE;
   }
 
-  printf("\nGross pay: %d\n", grossPay);
+  // Calculate taxes and net pay
+  taxes = calculate_tax(grossPay);
+  netPay = grossPay - taxes;
+
+  printf("\nGross pay: $%.2f\n", grossPay);
+  printf("Taxes: $%.2f\n", taxes);
+  printf("Net pay: $%.2f\n", netPay);
 
   return 0;
+}
+
+double calculate_tax(double wage) {
+  double tax = 0.0;
+
+  // First $300 at 15%
+  if (wage > 300) {
+    tax += 300 * 0.15;
+  } else {
+    tax += wage * 0.15;
+    return tax;
+  }
+
+  // Next $150 at 20%
+  if (wage > 450) {
+    tax += 150 * 0.20;
+  } else {
+    tax += (wage - 300) * 0.20;
+    return tax;
+  }
+
+  // Rest at 25%
+  tax += (wage - 450) * 0.25;
+
+  return tax;
 }
