bc94f41ddd59fad5fb506cae9f01afad8975b5e9
DannyBimma <dantrotman92@gmail.com>
2025-11-28 22:15:57 -0400

    .

---
+++ basic-programs/sizes.c
@@ -0,0 +1,6 @@
+/*
+ * Routine: A program that displays the byte sizes of various data-types
+ * in The Lord's language
+ * Author: DannyBimma
+ * Copyright: (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ */
