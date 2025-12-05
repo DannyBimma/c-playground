578aec5d25fea19c6b889c4ea7164044795b9dea
DannyBimma <dantrotman92@gmail.com>
2025-12-05 19:05:28 -0400

    Init source code file.

---
+++ basic-programs/guess.c
@@ -0,0 +1,12 @@
+/*
+ * Routine: A classic 'Guess the Number' game! The goal is to guess the
+ * correct number (0-420) that the game's code will randomly generate.
+ *
+ * Author: DannyBimma
+ *
+ * Copyright: (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
